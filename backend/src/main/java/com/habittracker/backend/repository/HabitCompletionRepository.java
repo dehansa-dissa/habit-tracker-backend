9b07e5e3ed44ad3d9792b3bcb1102975120c1dbf
dehansa-dissa <deha.dissa@gmail.com>
2026-04-03 17:01:30 +0530

    added HabitCompletionRepository

---
+++ backend/src/main/java/com/habittracker/backend/repository/HabitCompletionRepository.java
@@ -0,0 +1,13 @@
+package com.habittracker.backend.repository;
+
+import com.habittracker.backend.model.HabitCompletion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public interface HabitCompletionRepository extends JpaRepository<HabitCompletion, Long> {
+    List<HabitCompletion> findByHabitId(Long habitId);
+    Optional<HabitCompletion> findByHabitIdAndDate(Long habitId, LocalDate date);
+    boolean existsByHabitIdAndDate(Long habitId, LocalDate date);
+}
